7df2339164605000bc8e5bc6720b972b705a0140
brian-mulash <brianmulash03@gmail.com>
2023-05-09 04:33:39 +0300

    created menu configs

---
+++ frontend/src/configs/menu.configs.js
@@ -0,0 +1,74 @@
+import {
+   HiOutlineHome,
+   MdOutlineSlideshow,
+   MdOutlineLiveTv,
+   MdOutlineFavoriteBorder,
+   MdOutlineSearch,
+   MdOutlineRateReview,
+   MdOutlineLockReset,
+} from "react-icons/all";
+
+const main = [
+   {
+      display: "home",
+      path: "/",
+      icon: HiOutlineHome,
+      state: "home"
+   },
+
+   {
+      display: "movies",
+      path: "/movie",
+      icon: MdOutlineSlideshow,
+      state: "movie"
+   },
+
+   {
+      display: "tv series",
+      path: "/tv",
+      icon: MdOutlineLiveTv,
+      state: "tv"
+   },
+
+   {
+      display: "tv series",
+      path: "/tv",
+      icon: MdOutlineLiveTv,
+      state: "tv"
+   },
+
+   {
+      display: "search",
+      path: "/search",
+      icon: MdOutlineSearch,
+      state: "search"
+   },
+
+];
+
+const user = [
+   {
+      display: "favorites",
+      path: "/favorites",
+      icon: MdOutlineFavoriteBorder,
+      state: "favorite"
+   },
+
+   {
+      display: "reviews",
+      path: "/reviews",
+      icon: MdOutlineRateReview,
+      state: "reviews"
+   },
+
+   {
+      display: "password update",
+      path: "/password-update",
+      icon: MdOutlineLockReset,
+      state: "password.update"
+   },
+];
+
+const menuConfigs = {main, user};
+
+export default menuConfigs;
