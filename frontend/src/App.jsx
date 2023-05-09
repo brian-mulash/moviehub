1925da0b99cc70d78c7d880124553c4d54fa0b41
brian-mulash <brianmulash03@gmail.com>
2023-05-09 04:12:20 +0300

    created theme and ui configs for the client app part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
 import './scss/App.scss'
+import { ThemeProvider } from '@mui/material/styles';
+import { useSelector } from 'react-redux';
+import themeConfigs from './configs/theme.configs';
+import { ToastContainer } from "react-toastify"
+import { CssBaseline } from '@mui/material/CssBaseline';
 
 function App() {
 
-  return (
-    <>
-      Let's build a movie app
-    </>
-  )
-}
+  const { themeMode } = useSelector((state) => state.themeMode);
 
-export default App
+  return (
+    <ThemeProvider theme = {themeConfigs.custom({mode: themeMode})}>
+      {/* config toastify */}
+      <ToastContainer position='bottom-left' autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick pauseOnFocusLoss pauseOnHover theme={themeMode}/>
+
+      {/* mui reset css */}
+      <CssBaseline />
+    </ThemeProvider>
+  )
+};
+
+export default App;
